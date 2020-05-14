e4dba031a443b679019185421486d5e780fb0573
John Rivard <johnri@outlook.com>
2020-05-14 21:47:30 +0000

    delete old script from within codespace

--- cascade/CustomScriptForLinux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# CustomScriptForLinux.sh
-# $1 base64 encoded script, passed in from ARM template
-# $2..., arguments passed in from ARM template
-pushd ~
-echo $1 | base64 -d > _CustomScriptForLinux.sh
-chmod +x _CustomScriptForLinux.sh
-./_CustomScriptForLinux.sh "${@:2}"
-popd
